d15431c1702c2053e56b12c5fa3b17b2ab9b5568
Andre Correa <andre.correa@oracle.com>
2024-07-22 19:18:25 -0300

    chore: security zones commented out

--- variables_sec_main.tf
+++ variables_sec_main.tf
@@ -4,18 +4,18 @@
 # ------------------------------------------------------
 # ----- Security Zones
 # ------------------------------------------------------
-variable "enable_security_zones" {
-    type        = bool
-    default     = false
-    description = "Determines if Security Zones are enabled in Landing Zone. When set to true, the Security Zone is enabled for the enclosing compartment. If no enclosing compartment is used, then the Security Zone is not enabled."
-  }
+# variable "enable_security_zones" {
+#     type        = bool
+#     default     = false
+#     description = "Determines if Security Zones are enabled in Landing Zone. When set to true, the Security Zone is enabled for the enclosing compartment. If no enclosing compartment is used, then the Security Zone is not enabled."
+#   }
   
-  variable "sz_security_policies" {
-    type = list
-    default = []
-    description =  "List of Security Zones Policy OCIDs to add to security zone recipe. To get a Security Zone policy OCID use the oci cli:  oci cloud-guard security-policy-collection list-security-policies --compartment-id <tenancy-ocid>."
-    validation {
-      condition = length([for e in var.sz_security_policies : e if length(regexall("ocid1.securityzonessecuritypolicy.*", e)) > 0]) == length(var.sz_security_policies)
-      error_message = "Validation failed for sz_security_policies must be a valid Security Zone Policy OCID.  To get a Security Zone policy OCID use the oci cli:  oci cloud-guard security-policy-collection list-security-policies --compartment-id <tenancy-ocid>."
-    }
-  }
+#   variable "sz_security_policies" {
+#     type = list
+#     default = []
+#     description =  "List of Security Zones Policy OCIDs to add to security zone recipe. To get a Security Zone policy OCID use the oci cli:  oci cloud-guard security-policy-collection list-security-policies --compartment-id <tenancy-ocid>."
+#     validation {
+#       condition = length([for e in var.sz_security_policies : e if length(regexall("ocid1.securityzonessecuritypolicy.*", e)) > 0]) == length(var.sz_security_policies)
+#       error_message = "Validation failed for sz_security_policies must be a valid Security Zone Policy OCID.  To get a Security Zone policy OCID use the oci cli:  oci cloud-guard security-policy-collection list-security-policies --compartment-id <tenancy-ocid>."
+#     }
+#   }
