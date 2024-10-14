0b79c7e45ec92028f20c9ef79099e561f3688d58
Andre Correa <andre.correa@oracle.com>
2024-10-14 18:28:27 -0300

    feat: compute and network modules references updated

--- net_nfw.tf
+++ net_nfw.tf
@@ -200,7 +200,7 @@ locals {
 
 module "lz_firewall_appliance" {
   count                   = local.firewall_options[var.hub_vcn_deploy_net_appliance_option] != "NO" ? 1 : 0
-  source                  = "github.com/oci-landing-zones/terraform-oci-modules-workloads//cis-compute-storage?ref=release-0.1.7b"
+  source                  = "github.com/oci-landing-zones/terraform-oci-modules-workloads//cis-compute-storage?ref=v0.1.7"
   instances_configuration = local.instances_configuration
   providers = {
     oci                                  = oci.home
@@ -210,7 +210,7 @@ module "lz_firewall_appliance" {
 
 module "lz_nlb" {
   count             = local.firewall_options[var.hub_vcn_deploy_net_appliance_option] != "NO" ? 1 : 0
-  source            = "github.com/oci-landing-zones/terraform-oci-modules-networking//modules/nlb?ref=v0.6.9"
+  source            = "github.com/oci-landing-zones/terraform-oci-modules-networking//modules/nlb?ref=v0.7.0"
   nlb_configuration = local.nlb_configuration
 }
 
