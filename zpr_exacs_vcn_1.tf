a09253c1f39b846ff26d3be18a8ef287cd764f79
rory_nguyen <rory.nguyen@oracle.com>
2024-11-19 21:13:22 +0000

    fix: add exa hub vcn policy

--- zpr_exacs_vcn_1.tf
+++ zpr_exacs_vcn_1.tf
@@ -41,7 +41,7 @@ locals {
     ZPR-POLICY-EXA-1 = {
       description = "Core Landing Zone ZPR policy for ${var.exa_vcn1_name != null ? "${var.service_label}-${var.exa_vcn1_name}-zpr-policy" : "${var.service_label}-exa-vcn-1-zpr-policy"}."
       name        = var.exa_vcn1_name != null ? "${var.service_label}-${var.exa_vcn1_name}-zpr-policy" : "${var.service_label}-exa-vcn-1-zpr-policy"
-      statements = concat(local.exa_1_zpr_grants, local.exa_1_to_exa_2_zpr_grants, local.exa_1_to_exa_3_zpr_grants, local.exa_1_to_tt_1_zpr_grants, local.exa_1_to_tt_2_zpr_grants, local.exa_1_to_tt_3_zpr_grants)
+      statements = concat(local.exa_1_zpr_grants, local.exa_1_hub_zpr_grants, local.exa_1_to_exa_2_zpr_grants, local.exa_1_to_exa_3_zpr_grants, local.exa_1_to_tt_1_zpr_grants, local.exa_1_to_tt_2_zpr_grants, local.exa_1_to_tt_3_zpr_grants)
     }
   } : {}
 }
