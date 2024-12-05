5b8e7cb44061a70a941fdcfc24dfe2bc7ca7a1ac
Rory Nguyen <rory.nguyen@oracle.com>
2024-12-05 15:50:32 -0800

    add extra zpr policies for hub vcn and update exadata nsg rules for mgmt cidrs

--- zpr_hub_vcn.tf
+++ zpr_hub_vcn.tf
@@ -1,7 +1,13 @@
 locals {
 
   hub_zpr_grants = local.hub_with_vcn ? [
-    "in ${local.zpr_namespace_name}.net:hub-vcn VCN allow '${coalesce(var.hub_vcn_mgmt_subnet_cidr, cidrsubnet(var.hub_vcn_cidrs[0], 2, 3))}' to connect to ${local.zpr_namespace_name}.bastion:${local.zpr_label} endpoints with protocol='tcp/22'"
+    "in ${local.zpr_namespace_name}.net:hub-vcn VCN allow '${coalesce(var.hub_vcn_mgmt_subnet_cidr, cidrsubnet(var.hub_vcn_cidrs[0], 2, 3))}' to connect to ${local.zpr_namespace_name}.bastion:${local.zpr_label} endpoints with protocol='tcp/22'",
+    "in ${local.zpr_namespace_name}.net:hub-vcn VCN allow ${local.zpr_namespace_name}.bastion:${local.zpr_label} to connect to 'osn-services-ip-addresses' with protocol='tcp/443'"
+  ] : []
+
+  hub_on_prem_zpr_grants = local.hub_with_vcn ? [
+    for cidr in var.bastion_jump_host_allow_list : ##UPDATE IN FUTURE?
+    "in ${local.zpr_namespace_name}.net:hub-vcn VCN allow '${cidr}' to connect to ${local.zpr_namespace_name}.bastion:${local.zpr_label} endpoints with protocol='tcp/22'"
   ] : []
 
   hub_to_tt_1_zpr_grants = local.hub_with_vcn && local.add_tt_vcn1 && var.deploy_tt_vcn1_bastion_subnet ? [
@@ -13,7 +19,9 @@ locals {
   ] : []
 
   hub_to_tt_3_zpr_grants = local.hub_with_vcn && local.add_tt_vcn3 && var.deploy_tt_vcn3_bastion_subnet ? [
+    "in ${local.zpr_namespace_name}.net:hub-vcn VCN allow ${local.zpr_namespace_name}.bastion:${local.zpr_label} endpoints to connect to '${coalesce(var.tt_vcn3_bastion_subnet_cidr, cidrsubnet(var.tt_vcn3_cidrs[0], 9, 96))}' with protocol='tcp/22'",
     "in ${local.zpr_namespace_name}.net:hub-vcn VCN allow ${local.zpr_namespace_name}.bastion:${local.zpr_label} endpoints to connect to '${coalesce(var.tt_vcn3_bastion_subnet_cidr, cidrsubnet(var.tt_vcn3_cidrs[0], 9, 96))}' with protocol='tcp/22'"
+
   ] : []
 
   hub_to_exa_1_zpr_grants = local.hub_with_vcn && local.add_exa_vcn1 ? [
