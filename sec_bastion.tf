de428b49587308c7f0c0cfa299e15d6a487b7c1b
Niya Ma <bellatrix.mny@gmail.com>
2024-12-09 11:19:55 -0800

    fix duplicate module name

--- sec_bastion.tf
+++ sec_bastion.tf
@@ -94,15 +94,17 @@ module "lz_bastion" {
   source                = "github.com/oci-landing-zones/terraform-oci-modules-security//bastion?ref=v0.1.9"
   bastion_configuration = local.bastion_configuration
   count = local.deploy_bastion_service ? 1 : 0
+
 }
 
 module "lz_bastion_jump_host" {
   source = "github.com/oci-landing-zones/terraform-oci-modules-workloads//cis-compute-storage?ref=v0.1.8"
-  depends_on = [time_sleep.wait_on_services_policy]
+  depends_on = [bastion_time_sleep.wait_on_services_policy]
   count = ( local.hub_with_vcn == true && deploy_bastion_jump_host == true) ? 1 : 0
+
   providers = {
-    oci      = oci
-    oci.home = oci.home
+    oci                                  = oci.home
+    oci.block_volumes_replication_region = oci.home
   }
   
   instances_configuration = local.jump_host_instances_configuration
@@ -111,12 +113,12 @@ module "lz_bastion_jump_host" {
 # ----------------------------------------------------------------------------
 # -- Creating time sleep delays to slow down resource creation
 # ----------------------------------------------------------------------------
-resource "time_sleep" "wait_on_compartments" {
+resource "bastion_time_sleep" "wait_on_compartments" {
   depends_on      = [module.lz_compartments]
   create_duration = "70s"
 }
 
-resource "time_sleep" "wait_on_services_policy" {
+resource "bastion_time_sleep" "wait_on_services_policy" {
   depends_on      = [module.lz_policies]
   create_duration = "70s"
 }
