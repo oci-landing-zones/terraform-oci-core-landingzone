8a3522dc6a23f1858d346cc65b1d7a9ec4599cda
Andre Correa <andre.correa@oracle.com>
2024-12-16 15:57:33 -0300

    fix: redudant condition removed

--- net_nfw.tf
+++ net_nfw.tf
@@ -210,7 +210,7 @@ locals {
     }
     } : null
     
-    network_firewall_network_configuration =  local.chosen_firewall_option != "NO" && local.chosen_firewall_option == "OCINFW" ? {
+    network_firewall_network_configuration = local.chosen_firewall_option == "OCINFW" ? {
         #default_enable_cis_checks = false
         network_configuration_categories = {
             native_stack = {
