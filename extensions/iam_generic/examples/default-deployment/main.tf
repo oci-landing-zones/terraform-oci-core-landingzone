a6b9d4e2538203dbd5caa070d13f9f1a18cb0db8
mianguye <mia.nguyen@oracle.com>
2025-08-19 17:22:38 -0700

    Revert

--- extensions/iam_generic/examples/default-deployment/main.tf
+++ extensions/iam_generic/examples/default-deployment/main.tf
@@ -6,16 +6,24 @@
 module "test_iam" {
   source = "../../"
 
-  workload_compartment_name = var.workload_compartment_name
-  service_label             = var.service_label
-  parent_compartment_ocid   = var.parent_compartment_ocid
-  isolate_workload          = var.isolate_workload
-  network_compartment_ocid  = var.network_compartment_ocid
-  security_compartment_ocid = var.security_compartment_ocid
-  tenancy_ocid              = var.tenancy_ocid
-  user_ocid                 = var.user_ocid
-  fingerprint               = var.fingerprint
-  private_key_path          = var.private_key_path
-  private_key_password      = var.private_key_password
-  region                    = var.region
+  # ------------------------------------------------------
+  # ----- Tenancy Connectivity Variables
+  # ------------------------------------------------------
+  tenancy_ocid         = "ocid1.tenancy.oc1..aaaaaaaaxbchsnzhdxyoewmoqiqzvltba2ri7gijhbd2z5ybpgorv7yhxeeq" # Get this from OCI Console (after logging in, go to top-right-most menu item and click option "Tenancy: <your tenancy name>").
+  user_ocid            = "ocid1.user.oc1..aaaaaaaap3zy2bju3g2qunqmsaiod6xfiiskwaqx5epvhdrw7u4evxfztrfq" # Get this from OCI Console (after logging in, go to top-right-most menu item and click option "My profile").
+  fingerprint          = "26:6a:a9:20:bb:6d:40:98:66:87:d2:26:fb:1d:e9:d4" # The fingerprint can be gathered from your user account. In the "My profile page, click "API keys" on the menu in left hand side.
+  private_key_path     = "~/.oci.miakey.pem" # This is the full path on your local system to the API signing private key.
+  private_key_password = "" # This is the password that protects the private key, if any.
+  region               = "us-ashburn-1" # The region name.
+
+
+  #---------------------------------------
+  # ----- Input Variables
+  #---------------------------------------
+  workload_compartment_name = ""    # Default is "workload-cmp"
+  service_label             = "test"    # Required. A unique label that gets prepended to all resources deployed by the Landing Zone. Max length: 15 characters.
+  parent_compartment_ocid   = "ocid1.drg.oc1.iad.aaaaaaaa3hoezjafdk2f7nav4noozjc4nbpk4gok3vjjkifroas4u6kzwxrq"    # The ocid of the parent compartment of the Landing Zone deployed
+  isolate_workload          = false # The workload uses an isolated approach where network will be contained within the workload compartment instead of a shared network compartment.
+  network_compartment_ocid  = "ocid1.drg.oc1.iad.aaaaaaaa3hoezjafdk2f7nav4noozjc4nbpk4gok3vjjkifroas4u6kzwxrq"    # This value is required if isolate_workload = false. Ensure that the selected network compartment is within the selected parent compartment.
+  security_compartment_ocid = "ocid1.drg.oc1.iad.aaaaaaaa3hoezjafdk2f7nav4noozjc4nbpk4gok3vjjkifroas4u6kzwxrq"    # Ensure that the selected security compartment is within the selected parent compartment.
 }
