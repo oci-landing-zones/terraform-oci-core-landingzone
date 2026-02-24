762d7e33dc5f4c8b0881a191232e5a5a998e61da
Andre Correa <andre.correa@oracle.com>
2026-02-24 16:55:47 -0300

    fix: typo

--- net_oke_vcn_2.tf
+++ net_oke_vcn_2.tf
@@ -761,7 +761,7 @@ locals {
               local.oke_vcn_2_to_workers_subnet_cross_vcn_egress,
               local.oke_vcn_2_to_pods_subnet_cross_vcn_egress
             ),
-            ingress_rules = { for cidr_port_pair in local.oke_vcn2_external_allowed_cidrs_to_ports_into_services_tier : "INGRESS-FROM-${split(",",cidr_port_pair)[0])}-ON-${split(",",cidr_port_pair)[1]}-RULE" => {
+            ingress_rules = { for cidr_port_pair in local.oke_vcn2_external_allowed_cidrs_to_ports_into_services_tier : "INGRESS-FROM-${split(",",cidr_port_pair)[0]}-ON-${split(",",cidr_port_pair)[1]}-RULE" => {
               description  = "Ingress from ${split(",",cidr_port_pair)[0]} over ${split(":",split(",",cidr_port_pair)[1])[0]} on port ${split(":",split(",",cidr_port_pair)[1])[1]}."
               stateless    = false
               protocol     = split(":",split(",",cidr_port_pair)[1])[0]
