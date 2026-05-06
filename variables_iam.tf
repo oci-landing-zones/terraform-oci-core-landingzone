2664bddc4b95dfee125bfa0d972352ab5b1a52d6
Andre Correa <andre.correa@oracle.com>
2026-05-06 15:34:35 -0300

    feat: validation added to custom cmp names

--- variables_iam.tf
+++ variables_iam.tf
@@ -38,31 +38,55 @@ variable "custom_enclosing_compartment_name" {
   type        = string
   default     = null
   description = "Custom name of the enclosing compartment."
+  validation {
+    condition = var.custom_enclosing_compartment_name == null || (length(var.custom_enclosing_compartment_name) > 3 && !startswith(var.custom_enclosing_compartment_name, " "))
+    error_message = "Validation failed for variable custom_enclosing_compartment_name: it must be null, or a string longer than 3 characters that does not start with a space."
+  }
 }
 variable "custom_network_compartment_name" {
   type        = string
   default     = null
   description = "Custom name of the network compartment."
+  validation {
+    condition = var.custom_network_compartment_name == null || (length(var.custom_network_compartment_name) > 3 && !startswith(var.custom_network_compartment_name, " "))
+    error_message = "Validation failed for variable custom_network_compartment_name: it must be null, or a string longer than 3 characters that does not start with a space."
+  }
 }
 variable "custom_security_compartment_name" {
   type        = string
   default     = null
   description = "Custom name of the security compartment."
+  validation {
+    condition = var.custom_security_compartment_name == null || (length(var.custom_security_compartment_name) > 3 && !startswith(var.custom_security_compartment_name, " "))
+    error_message = "Validation failed for variable custom_security_compartment_name: it must be null, or a string longer than 3 characters that does not start with a space."
+  }
 }
 variable "custom_app_compartment_name" {
   type        = string
   default     = null
   description = "Custom name of the app compartment."
+  validation {
+    condition = var.custom_app_compartment_name == null || (length(var.custom_app_compartment_name) > 3 && !startswith(var.custom_app_compartment_name, " "))
+    error_message = "Validation failed for variable custom_app_compartment_name: it must be null, or a string longer than 3 characters that does not start with a space."
+  }
 }
 variable "custom_database_compartment_name" {
   type        = string
   default     = null
   description = "Custom name of the database compartment."
+  validation {
+    condition = var.custom_database_compartment_name == null || (length(var.custom_database_compartment_name) > 3 && !startswith(var.custom_database_compartment_name, " "))
+    error_message = "Validation failed for variable custom_database_compartment_name: it must be null, or a string longer than 3 characters that does not start with a space."
+  }
 }
 variable "custom_exainfra_compartment_name" {
   type        = string
   default     = null
   description = "Custom name of the exadata infrastructure compartment."
+  validation {
+    condition = var.custom_exainfra_compartment_name == null || (length(var.custom_exainfra_compartment_name) > 3 && !startswith(var.custom_exainfra_compartment_name, " "))
+    error_message = "Validation failed for variable custom_exainfra_compartment_name: it must be null, or a string longer than 3 characters that does not start with a space."
+  }
 }
 # ------------------------------------------------------
 # ----- IAM - Identity Domains
